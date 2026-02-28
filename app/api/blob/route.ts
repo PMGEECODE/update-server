3ff0276de3bad5834ae12174017fc0619073dc00
George Michael Pussl <codetst.dev@gmail.com>
2026-02-28 20:09:29 +0300

    updates 121 b2

--- app/api/blob/route.ts
+++ app/api/blob/route.ts
@@ -9,6 +9,7 @@ export async function POST(request: Request): Promise<NextResponse> {
     const jsonResponse = await handleUpload({
       body,
       request,
+      token: process.env.BLOB_READ_WRITE_TOKEN,
       onBeforeGenerateToken: async () => {
         const supabase = await createClient();
         const {
